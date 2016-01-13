473efa4c2955bf1265bf085182a6ffbc9026ce12
Sebastian Ruf <Sebastian Ruf>
2016-01-13 16:20:28 +0100

    topic newTopic redirect after submit

--- src/AppBundle/Controller/TopicController.php
+++ src/AppBundle/Controller/TopicController.php
@@ -74,7 +74,7 @@ class TopicController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($topic);
             $em->flush();
-            return $this->redirect($this->generateUrl('watchTopic/' . $topic->getId()));
+            return $this->redirect($this->generateUrl('watchTopic', array('id' => $topic->getId())));
         }
 
         return $this->render('Topic/new.html.twig', array(
